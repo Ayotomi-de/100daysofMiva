df99f879e44f2ed4b6663c46f1afe2628a976b9e
Ayotomide <ayotomide.toluwani@gmail.com>
2024-08-25 09:36:52 +0100

    Update to script.js

---
+++ Day 3 and 4/script.js
@@ -0,0 +1,8 @@
+let js = "amazing";
+if (js === "amazing") alert("Javascript is FUN!");
+
+54 + 60 - 13
+    
+```This will show in the console on the web browser```
+    
+console.log(54 + 60 - 13);
